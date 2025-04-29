e120cfa1658bbc1d8ebb9e91471b02ea5416e67d
PRIYANSHU700 <140921702+PRIYANSHU700@users.noreply.github.com>
2025-04-30 00:36:16 +0530

    Update Homesection3.js

--- components/Homesection3.js
+++ components/Homesection3.js
@@ -14,7 +14,7 @@ const Homesection3 = () => {
 
          <Card2_right cimg={projectimg3} cname="Youtube Clone" cdescription="I built this project using Rapid API" cvisit="link" ccode="https://github.com/PRIYANSHU700/Youtube-clone.git"/> 
 
-        <Card1_left cimg={projectimg2} cname="Project 3" cdescription="work in progress " cvisit="applink" ccode="gitlink"/> 
+         <Card1_left cimg={projectimg2} cname="Crop Yield Prediction System" cdescription="Developed a Crop Yield Prediction System using Random Forest and XGBoost, achieving 95.51% accuracy with XGBoost. Built a Streamlit interface enabling farmers to input data and receive real-time yield predictions for informed decision-making. " cvisit="https://crop-yield-prediction-system77.streamlit.app/" ccode="https://github.com/PRIYANSHU700/Crop-Yield-Prediction-System"/>
 
         <Link href="/projects">
         <button className={styles.mybtn}>Show All Projects</button>
