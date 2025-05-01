5c422e4fde8cadeefa8f722cac1fab7aa1cd9a98
PRIYANSHU700 <140921702+PRIYANSHU700@users.noreply.github.com>
2025-05-01 17:24:38 +0530

    Update Homesection1.js

--- components/Homesection1.js
+++ components/Homesection1.js
@@ -26,7 +26,7 @@ const Homesection1 = () => {
         </p>
         
         <button onClick={() => window.location.href = '/about'} className={styles.ctaButton}>  Know More <span className={styles.pointer}>👈</span></button>
-        <button onClick={() => window.location.href = '/about'} className={styles.ctaButton}>  Download CV <span className={styles.pointer}></span></button>
+        <button onClick={() => window.location.href = '/about'} className={styles.ctaButton}>  Download CV <span className={styles.pointer}>👈</span></button>
 
 
         <div className={styles.socialLinks}>
