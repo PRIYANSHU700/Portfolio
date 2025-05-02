222795035fe3e78d8bbfac52e3cdedf5be41d97c
PRIYANSHU700 <140921702+PRIYANSHU700@users.noreply.github.com>
2025-05-02 15:30:28 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,46 +4,54 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { useState } from 'react'
 import { useRouter } from 'next/router'
+
 const Navbar = () => {
-  const [navactive,setnavactive] = useState('false');
+  const [navActive, setNavActive] = useState(false);
   const router = useRouter();
-  const handlenavbarclick = ()=>{
-    if(navactive=='false'){
-      setnavactive('true')
-    }
-    else{
-      setnavactive('false')
-    }
-  }
+
+  const handleNavbarClick = () => {
+    setNavActive(!navActive);
+  };
+
+  const NavLinks = () => (
+    <>
+      <Link href='/'><button className={router.pathname === '/' ? styles.pageactive : ''}>Home</button></Link>
+      <Link href='/about'><button className={router.pathname === '/about' ? styles.pageactive : ''}>About</button></Link>
+      <Link href='/projects'><button className={router.pathname === '/projects' ? styles.pageactive : ''}>Projects</button></Link>
+      <Link href='/contact'><button className={router.pathname === '/contact' ? styles.pageactive : ''}>Contact</button></Link>
+    </>
+  );
+
   return (
     <div className={styles.navouter}>
-        <div className={styles.left}>
-            <Image src={signature} className={styles.sign} height={60} width={120} />
+      <div className={styles.left}>
+        <Image src={signature} className={styles.sign} height={60} width={120} alt="Signature" />
+      </div>
 
-        </div>
-        <div className={navactive=='true'?`${styles.nav_toggler} ${styles.navactive}`:styles.nav_toggler} onClick={handlenavbarclick}>
-            <span></span>
-          
-        </div>
-        <div className={styles.right}>
-            <Link href='/'><button className={router.pathname=='/'?styles.pageactive:''}>Home </button></Link>
-            <Link href='/about'><button className={router.pathname=='/about'?styles.pageactive:''}>About</button></Link>
-            <Link href='/projects'><button className={router.pathname=='/projects'?styles.pageactive:''}>Projects</button></Link>
-            <Link href='/contact'><button className={router.pathname=='/contact'?styles.pageactive:''}>Contact</button></Link>
-        </div>
-        {
-          navactive == 'true' ?
-            <div className={styles.right1}>
-                <Link href='/'><button className={router.pathname=='/'?styles.pageactive:''}>Home </button></Link>
-                <Link href='/about'><button className={router.pathname=='/about'?styles.pageactive:''}>About</button></Link>
-                <Link href='/projects'><button className={router.pathname=='/projects'?styles.pageactive:''}>Projects</button></Link>
-                <Link href='/contact'><button className={router.pathname=='/contact'?styles.pageactive:''}>Contact</button></Link>
-              </div>
-              :
-              <></>
-        }
-    </div> 
-  )
-}
+      <div
+        className={`${styles.nav_toggler} ${navActive ? styles.navactive : ''}`}
+        onClick={handleNavbarClick}
+        aria-label="Toggle navigation"
+        aria-expanded={navActive}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
 
-export default Navbar
+      {/* Desktop Nav */}
+      <div className={styles.right}>
+        <NavLinks />
+      </div>
+
+      {/* Mobile Nav */}
+      {navActive && (
+        <div className={styles.right1}>
+          <NavLinks />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
